f1580259b1f80ee857cf3a2a248cc36192c4b6d1
crossjs <liwenfu@crossjs.com>
2015-04-17 11:47:21 +0800

    update

---
+++ README.md
@@ -0,0 +1,18 @@
+# nd-user
+
+[![spm version](http://spm.crossjs.com/badge/nd-user)](http://spm.crossjs.com/package/nd-user)
+
+> UC Authorization
+
+## 安装
+
+```bash
+$ spm install nd-user --save
+```
+
+## 使用
+
+```js
+var user = require('nd-user');
+// use user
+```
---
+++ index.js
@@ -0,0 +1,49 @@
+/**
+ * @module: nd-user
+ * @author: crossjs <liwenfu@crossjs.com> - 2015-04-17 11:39:53
+ */
+
+'use strict';
+
+var Storage = require('nd-storage');
+
+module.exports = {
+
+  tokenKey: 'USER_DATA',
+
+  userdata: null,
+
+  getData: function(key) {
+    var userdata = this.userdata;
+
+    if (!userdata) {
+      // 本地存储
+      userdata = Storage.get(this.tokenKey);
+
+      if (userdata) {
+        userdata = this.userdata = JSON.parse(userdata);
+      }
+    }
+
+    if (key && userdata) {
+      return userdata[key];
+    }
+
+    return userdata;
+  },
+
+  /**
+   * 设置或清除 userdata
+   * @param {object} userdata userdata值
+   */
+  setData: function(userdata) {
+    this.userdata = userdata;
+
+    if (userdata === null) {
+      Storage.remove(this.tokenKey);
+    } else {
+      Storage.set(this.tokenKey, JSON.stringify(userdata));
+    }
+  }
+
+};
